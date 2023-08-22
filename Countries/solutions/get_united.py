32e6179854d7cf50daf63d14b6228f865f8dd6b2
christopherkeim <christopher.jl.keim@gmail.com>
2023-08-22 15:48:33 -0400

    First module test and module implemented.

---
+++ Countries/solutions/get_united.py
@@ -0,0 +1,28 @@
+"""
+This function should consume the countries.txt data and return all of the words that contain "United"
+"""
+import logging
+
+logger = logging.getLogger()
+
+
+def get_united(data: list[str]) -> list[str]:
+    """Return a list of strings holding 'United' countries"""
+
+    # Check if we have the correct paramter input type
+    logger.info("Checking data type of input.")
+    if type(data) != list and type(data[0]) != str:
+        raise TypeError("This function requires a list of strings as input.")
+
+    logger.info("Generating list of words with 'United'.")
+
+    # Initialize an empty list
+    united_words: list[str] = []
+
+    # Loop through each word in our data
+    for word in data:
+        # Check if the word contains 'United'
+        if "United" in word:
+            united_words.append(word)
+
+    return united_words
