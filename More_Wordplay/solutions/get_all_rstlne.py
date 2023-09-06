7bfcdb678a7e60efc8b95cd4a98873c9ed930d58
Christopher Keim <christopher.jl.keim@gmail.com>
2023-09-05 22:10:40 -0400

    Documentation cleanup.

--- More_Wordplay/solutions/get_all_rstlne.py
+++ More_Wordplay/solutions/get_all_rstlne.py
@@ -34,40 +34,6 @@ ROOT: Path = Path("More_Wordplay")
 SCRABBLE_WORDS: Path = ROOT / "tests" / "fixtures" / "sowpods.txt"
 
 
-def runner(test: bool = False) -> None:
-    # First implementation as list[str]
-    scrabble_words1: list[str] = load_data_from_file(SCRABBLE_WORDS)
-
-    first_start_time: float = time.time()
-    results1: list[str] = get_all_rstlne(scrabble_words1)
-    first_end_time: float = time.time() - first_start_time
-
-    # Second implementation as dict[set[str]]
-    scrabble_words2: dict[set[str]] = load_data_from_file_as_dict_of_sets(
-        SCRABBLE_WORDS
-    )
-
-    second_start_time: float = time.time()
-    results2: list[str] = get_all_rstlne_using_dict_of_sets(scrabble_words2)
-    second_end_time: float = time.time() - second_start_time
-
-    if results1 == results2:
-        logger.info(f"{'#'*8} ✨ get_all_rstlne.py results ✨ {'#'*8}\n")
-        for result in results1:
-            logger.info(result)
-        logger.info(
-            f"""\n\nTime taken for first implementation (as LIST[STR]): 
-            {first_end_time} seconds
-            """
-        )
-        logger.info(
-            f"""Time taken for second implementation(as DICT[SET[STR]]): 
-            {second_end_time} seconds\n
-            """
-        )
-        logger.info(f"Improvement: {(second_end_time / first_end_time) * 100}%")
-
-
 def load_data_from_file(file_path: str) -> list[str]:
     """
     Takes a file path as input, reads that file into memory, and
@@ -124,5 +90,31 @@ def get_all_rstlne_using_dict_of_sets(scrabble_words: dict[set[str]]) -> list[st
     return matches
 
 
+def main() -> None:
+    # First implementation as list[str]
+    scrabble_words1: list[str] = load_data_from_file(SCRABBLE_WORDS)
+
+    first_start_time: float = time.time()
+    results1: list[str] = get_all_rstlne(scrabble_words1)
+    first_end_time: float = time.time() - first_start_time
+
+    # Second implementation as dict[set[str]]
+    scrabble_words2: dict[set[str]] = load_data_from_file_as_dict_of_sets(
+        SCRABBLE_WORDS
+    )
+
+    second_start_time: float = time.time()
+    results2: list[str] = get_all_rstlne_using_dict_of_sets(scrabble_words2)
+    second_end_time: float = time.time() - second_start_time
+
+    if results1 == results2:
+        logger.info(f"{'#'*8} ✨ get_all_rstlne.py results ✨ {'#'*8}\n")
+        for result in results1:
+            logger.info(result)
+        logger.info(f"\n\nTime taken for LIST[STR]: {first_end_time} seconds")
+        logger.info(f"Time taken for DICT[SET[STR]]: {second_end_time} seconds\n")
+        logger.info(f"Improvement: {(second_end_time / first_end_time) * 100}%")
+
+
 if __name__ == "__main__":
-    runner()
+    main()
